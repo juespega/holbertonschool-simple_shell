90c9e86dcbb81351a365d59f592795bb9070d7d9
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-21 14:17:25 -0500

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -5,7 +5,14 @@
 #include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
-char **tokens = malloc(sizeof(char*) * BUFFER_SIZE)
+
+void remove_newline(char *str) 
+{   char *newline;
+    newline = strchr(str, '\n');
+    if (newline != NULL) {
+        *newline = '\0';
+    }
+}
 int main(void) 
 {
     ssize_t char_read;
@@ -14,7 +21,9 @@ int main(void)
     char *token;
     pid_t pid;
     int num_tokens;
+    char *tokens[BUFFER_SIZE];
     
+
     while (1) {
         printf("ajr> ");
 
@@ -24,14 +33,10 @@ int main(void)
            return (-1);
         }
 
-        line[strcspn(line, "\n")] = '\0';
+       remove_newline(line);
 
-        char **tokens = malloc(sizeof(char*) * BUFFER_SIZE);
-        if (tokens == NULL) {
-            perror("Error");
-            return (-1);
-        }
-
+      
+        
         num_tokens = 0;
         token = strtok(line, " \n");
         while (token != NULL) {
@@ -42,33 +47,28 @@ int main(void)
         tokens[num_tokens] = NULL;
 
         if (num_tokens == 0) {
-            free(tokens);
             continue;
         }
 
         pid = fork();
 
         if (pid == -1) {
-            free(tokens);
             return (-1);
         } else if (pid == 0) {
            
             if (execvp(tokens[0], tokens) == -1) {
                 perror("Error");
-                free(tokens);
                 return (-1);
             }
         } else {
             int status;
             if (wait(&status) == -1) {
-                free(tokens);
                 return (-1);
             }
         }
-
-        free(tokens);
     }
 
     free(line);
+
     return 0;
 }
