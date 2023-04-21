f721ca19670dca8b173c2aa8d19279c292eacbbd
juespega <juespega666@gmail.com>
2023-04-21 11:27:02 -0700

    simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -13,39 +13,44 @@ int main(void)
     size_t bytes_read = 0;
     char *token;
     pid_t pid;
-    
+
     while (1) {
         printf("ajr> ");
 
-        
         char_read = getline(&line, &bytes_read, stdin);
 
         if (char_read == -1) {
            return (-1);
         }
 
-        
         line[strcspn(line, "\n")] = '\0';
 
-        
-        token = strtok(line, " ");
+      
+        char *tokens[BUFFER_SIZE];
+        int num_tokens = 0;
+        token = strtok(line, " \n");
+        while (token != NULL) {
+            tokens[num_tokens] = token;
+            num_tokens++;
+            token = strtok(NULL, " \n");
+        }
+        tokens[num_tokens] = NULL;
 
-        if (token == NULL) {
+        if (num_tokens == 0) {
             continue;
         }
 
-        
         pid = fork();
 
         if (pid == -1) {
             return (-1);
         } else if (pid == 0) {
-            
-            if (execlp(token, token, (char *) NULL) == -1) {
-               return (-1);
+           
+            if (execvp(tokens[0], tokens) == -1) {
+                perror("Error");
+                return (-1);
             }
         } else {
-            
             int status;
             if (wait(&status) == -1) {
                 return (-1);
