94cdd69d4da510875d5b2af6ac6998cfd11dc398
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-04-22 20:11:36 -0500

    Update shell.h
    ok

--- shell.h
+++ shell.h
@@ -8,6 +8,10 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+#define MAX_COMMAND 10
+#define MAX_PATH_LEN 1024
+#define MAX_COMPAND 10
+
 void prompt(char **av, char **env);
 int main(int ac, char **av, char **env);
 
