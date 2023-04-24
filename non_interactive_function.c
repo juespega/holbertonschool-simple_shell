17058846a706789dcf23545048889b26930607f4
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-24 17:23:48 -0500

    Update non_interactive_function.c

--- non_interactive_function.c
+++ non_interactive_function.c
@@ -10,27 +10,30 @@ void non_interactive(void)
 {
     char *line = NULL;
     size_t n = 0;
-    char **current_command;
+    char **current_command = NULL; // inicializar a NULL
+    char **commands;
     int i;
+    int type_command;
 
     while (getline(&line, &n, stdin) != -1)
     {
         remove_newline(line);
         remove_comment(line);
-        char **commands = tokenizer(line, ";");
+        commands = tokenizer(line, ";");
         for (i = 0; commands[i] != NULL; i++)
         {
-            *current_command = tokenizer(commands[i], " ");
-            if (current_command[0] != NULL)
+            current_command = tokenizer(commands[i], " ");
+            if (current_command != NULL && current_command[0] != NULL)
             {
-                int type_command = parse_command(current_command[0]);
+                type_command = parse_command(current_command[0]);
                 initializer(current_command, type_command);
             }
-            free(current_command);
+            if (current_command != NULL) // asegurarse de que no se libere NULL
+                free(current_command);
         }
         free(commands);
     }
-    free(line);
+    if (line != NULL) // asegurarse de que no se libere NULL
+        free(line);
     exit(status);
 }
-
