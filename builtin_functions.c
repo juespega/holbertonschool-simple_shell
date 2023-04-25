b7e60a36ff4cb2a914e22522eff791a244957334
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-25 11:23:57 -0500

    Update builtin_functions.c

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,21 +1,21 @@
 #include "shell.h"
 
 /**
- *env - prints the current_environnement
- *@tokenized_command: command entered
+ * env - prints the current environment variables
+ * @tokenized_command: command entered
  *
- *Return: void
+ * Return: void
  */
 
 void env(char **tokenized_command __attribute__((unused)))
 {
-	int i;
+    int i;
 
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		print(environ[i], STDOUT_FILENO);
-		print("\n", STDOUT_FILENO);
-	}
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        print(environ[i], STDOUT_FILENO);
+        print("\n", STDOUT_FILENO);
+    }
 }
 
 /**
@@ -27,36 +27,38 @@ void env(char **tokenized_command __attribute__((unused)))
 
 void quit(char **tokenized_command)
 {
-	int num_token = 0, arg;
+    int num_token = 0, arg;
 
-	for (; tokenized_command[num_token] != NULL; num_token++)
-		;
-	if (num_token == 1)
-	{
-		free(tokenized_command);
-		free(line);
-		free(commands);
-		exit(status);
-	}
-	else if (num_token == 2)
-	{
-		arg = _atoi(tokenized_command[1]);
-		if (arg == -1)
-		{
-			print(shell_name, STDERR_FILENO);
-			print(": 1: exit: Illegal number: ", STDERR_FILENO);
-			print(tokenized_command[1], STDERR_FILENO);
-			print("\n", STDERR_FILENO);
-			status = 2;
-		}
-		else
-		{
-			free(line);
-			free(tokenized_command);
-			free(commands);
-			exit(arg);
-		}
-	}
-	else
-		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+    while (tokenized_command[num_token] != NULL)
+        num_token++;
+
+    if (num_token == 1)
+    {
+        free(tokenized_command);
+        free(line);
+        free(commands);
+        exit(status);
+    }
+    else if (num_token == 2)
+    {
+        arg = _atoi(tokenized_command[1]);
+        if (arg == -1)
+        {
+            print(shell_name, STDERR_FILENO);
+            print(": 1: exit: Illegal number: ", STDERR_FILENO);
+            print(tokenized_command[1], STDERR_FILENO);
+            print("\n", STDERR_FILENO);
+            status = 2;
+        }
+        else
+        {
+            free(line);
+            free(tokenized_command);
+            free(commands);
+            exit(arg);
+        }
+    }
+    else
+        print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
+
