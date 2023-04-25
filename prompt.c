8d2ad5e7adf66b9c80a626783b30fbe55afb6c74
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-04-25 16:51:35 -0500

    Update prompt.c
    ok

--- prompt.c
+++ prompt.c
@@ -1,5 +1,6 @@
 #include "shell.h"
-#define MAX_COMPAND 10
+#define MAX_COMMANDS 10
+
 /**
  * prompt - Display a shell prompt and execute commands
  * @av: Array of command line argument strings
@@ -15,21 +16,25 @@ void prompt(char **av, char **env)
     int i, j;
     int status;
     ssize_t num_char;
-    char *argv[MAX_COMPAND];
-    pid_t chil_pid;
+    char *argv[MAX_COMMANDS];
+    pid_t child_pid;
     extern char **environ;
     char *path_copy;
     char *dir;
+
     while (1)
     {
         if (isatty(STDIN_FILENO))
             printf("cisfun$ ");
+
         num_char = getline(&string, &n, stdin);
+
         if (num_char == -1)
         {
             free(string);
             exit(EXIT_SUCCESS);
         }
+
         i = 0;
         while (string[i])
         {
@@ -37,17 +42,22 @@ void prompt(char **av, char **env)
                 string[i] = 0;
             i++;
         }
+
         j = 0;
         argv[j] = strtok(string, " ");
+
         while (argv[j])
             argv[++j] = strtok(NULL, " ");
-        chil_pid = fork();
-        if (chil_pid == -1)
+
+        child_pid = fork();
+
+        if (child_pid == -1)
         {
             free(string);
             exit(EXIT_FAILURE);
         }
-        if (chil_pid == 0)
+
+        if (child_pid == 0)
         {
             if ((argv[0] == NULL) || strlen(argv[0]) == 0)
             {
@@ -55,36 +65,58 @@ void prompt(char **av, char **env)
             }
             if (execve(argv[0], argv, env) == -1)
             {
-                /**Verificar si el comando existe en las rutas especificadas en PATH**/
+                /* Verificar si el comando existe en las rutas especificadas en PATH */
                 char *path_value = NULL;
                 char *path_name = "PATH=";
                 char **envp = environ;
-                while (*envp != NULL) {
-                    if (strncmp(*envp, path_name, strlen(path_name)) == 0) {
+
+                while (*envp != NULL)
+                {
+                    if (strncmp(*envp, path_name, strlen(path_name)) == 0)
+                    {
                         path_value = strchr(*envp, '=') + 1;
                         break;
                     }
                     envp++;
                 }
-                if (path_value == NULL) {
+
+                if (path_value == NULL)
+                {
                     printf("No se encontró la variable de entorno PATH.\n");
                     exit(EXIT_FAILURE);
                 }
+
                 path_copy = strdup(path_value);
                 dir = strtok(path_copy, ":");
+
                 while (dir)
                 {
-                    char *cmd_path = malloc(strlen(dir) + strlen(argv[0]) + 2);
-                    sprintf(cmd_path, "%s/%s", dir, argv[0]);
-                    if (access(cmd_path, X_OK) == 0)
+                    struct stat sb;
+                    char cmd_path[BUFSIZ];
+                    int len = snprintf(cmd_path, BUFSIZ, "%s/%s", dir, argv[0]);
+
+                    if (len >= BUFSIZ)
                     {
-                        /**Ejecutar el comando si existe en PATH**/
+                        printf("Error: la ruta del comando es demasiado larga.\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    if (stat(cmd_path, &sb) == -1)
+                    {
+                        dir = strtok(NULL, ":");
+                        continue;
+                    }
+
+                    if (S_ISREG(sb.st_mode) && (sb.st_mode & S_IXUSR))
+                    {
+                        /* Ejecutar el comando si existe en PATH */
                         execve(cmd_path, argv, env);
                     }
-                    free(cmd_path);
+
                     dir = strtok(NULL, ":");
                 }
-                printf("%s: No funciona con este comando \n ", av[0]);
+
+                printf("%s: No se encontró el comando.\n", argv[0]);
                 free(path_copy);
                 exit(EXIT_FAILURE);
             }
@@ -95,3 +127,4 @@ void prompt(char **av, char **env)
         }
     }
 }
+
