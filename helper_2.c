53ee1cec104f5892d7a422cb294c99513608b346
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-25 11:46:58 -0500

    Update helper_2.c

--- helper_2.c
+++ helper_2.c
@@ -1,52 +1,26 @@
 #include "shell.h"
 
-
-/**
- *_strtok_r - tokenizes a string
- *@string: string to be tokenized
- *@delim: delimiter to be used to tokenize the string
- *@save_ptr: pointer to be used to keep track of the next token
- *
- *Return: The next available token
- */
-char *_strtok_r(char *string, char *delim, char **save_ptr)
+char *_strtok_r(char *s, char *delim, char **save_ptr)
 {
-	char *finish;
+	if (s == NULL)
+		s = *save_ptr;
 
-	if (string == NULL)
-		string = *save_ptr;
+	s += _strspn(s, delim);
 
-	if (*string == '\0')
+	if (*s == '\0')
 	{
-		*save_ptr = string;
+		*save_ptr = s;
 		return (NULL);
 	}
 
-	string += _strspn(string, delim);
-	if (*string == '\0')
-	{
-		*save_ptr = string;
-		return (NULL);
-	}
+	*save_ptr = s + _strcspn(s, delim);
 
-	finish = string + _strcspn(string, delim);
-	if (*finish == '\0')
-	{
-		*save_ptr = finish;
-		return (string);
-	}
+	if (**save_ptr != '\0')
+		*(*save_ptr)++ = '\0';
 
-	*finish = '\0';
-	*save_ptr = finish + 1;
-	return (string);
+	return (s);
 }
 
-/**
- * _atoi - changes a string to an integer
- * @s: the string to be changed
- *
- * Return: the converted int
- */
 int _atoi(char *s)
 {
 	unsigned int n = 0;
@@ -61,18 +35,11 @@ int _atoi(char *s)
 		else if (n > 0)
 			break;
 	} while (*s++);
+
 	return (n);
 }
 
-/**
- * _realloc - reallocates a memory block
- * @ptr: pointer to the memory previously allocated with a call to malloc
- * @old_size: size of ptr
- * @new_size: size of the new memory to be allocated
- *
- * Return: pointer to the address of the new memory block
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *temp_block;
 	unsigned int i;
@@ -82,60 +49,47 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		temp_block = malloc(new_size);
 		return (temp_block);
 	}
-	else if (new_size == old_size)
+
+	if (new_size == old_size)
 		return (ptr);
-	else if (new_size == 0 && ptr != NULL)
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
-	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *)ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
 
-	}
+	temp_block = malloc(new_size);
+
+	if (temp_block == NULL)
+		return (NULL);
+
+	for (i = 0; i < min(old_size, new_size); i++)
+		*((char *)temp_block + i) = *((char *)ptr + i);
+
+	free(ptr);
+	return (temp_block);
 }
 
-/**
- * ctrl_c_handler - handles the signal raised by CTRL-C
- * @signum: signal number
- *
- * Return: void
- */
 void ctrl_c_handler(int signum)
 {
 	if (signum == SIGINT)
 		print("\n($) ", STDIN_FILENO);
 }
 
-/**
- * remove_comment - removes/ignores everything after a '#' char
- * @input: input to be used
- *
- * Return: void
- */
 void remove_comment(char *input)
 {
 	int i = 0;
 
 	if (input[i] == '#')
 		input[i] = '\0';
+
 	while (input[i] != '\0')
 	{
 		if (input[i] == '#' && input[i - 1] == ' ')
 			break;
 		i++;
 	}
+
 	input[i] = '\0';
 }
-
-
