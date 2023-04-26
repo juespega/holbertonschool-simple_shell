4492f86dedfe9fe307b45627854e0eb9ae974f17
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-26 11:47:24 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,3 +9,13 @@ int main(int ac, char **av, char **env)
     }
   return 0;
 }
+int env_builtin(char **env)
+{
+    int i = 0;
+    while (env[i] != NULL)
+    {
+        printf("%s\n", env[i]);
+        i++;
+    }
+    return (0);
+}
