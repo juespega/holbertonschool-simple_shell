77c84b8d101840ea833ce991bfc3c70ed7cc2290
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-27 10:17:38 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,15 +1,22 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include <libgen.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 
 void prompt(char **av, char **env);
-int main(int ac, char **av, char **env);
-int env_builtin(char **env);
+
 
 #endif
+
