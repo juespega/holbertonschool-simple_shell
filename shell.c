b0fae30a6624bec0debd91f8bee49ff1aa393871
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-04-27 11:54:42 -0500

    Update shell.c
    ok

--- shell.c
+++ shell.c
@@ -10,3 +10,22 @@ int main(int ac, char **av, char **env)
   return 0;
 }
 
+/**
+ * env_builtin - prints all of the program's environment variables to the console.
+ * @env: Array of strings contains the environment variables of a program in the form of key=value 
+ *
+ * Description: contains the environment variables of a program in the form of key=value
+ * Return: 0
+ */
+
+
+int env_builtin(char **env)
+{
+    int i = 0;
+    while (env[i] != NULL)
+    {
+        printf("%s\n", env[i]);
+        i++;
+    }
+    return (0);
+}
