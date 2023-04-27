c566e0f37d2bb800ef3da302cc614fd9d2002540
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-04-27 12:21:20 -0500

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 #define MAX_COMMAND 10
-#define MAX_COMPAND 10
+
 
 /**
  * prompt - Display a shell prompt and execute commands
@@ -22,6 +22,9 @@ void prompt(char **av, char **env)
     extern char **environ;
     char *path_copy;
     char *dir;
+    char *path_value = NULL;
+    char *path_name = "PATH=";
+    char **envp = environ;
     while (1)
     {
         if (isatty(STDIN_FILENO))
@@ -55,12 +58,16 @@ void prompt(char **av, char **env)
             {
                 continue;
             }
+            if (strcmp(argv[0], "env") == 0)
+            {
+            env_builtin(env);
+            continue;
+            }
+
             if (execve(argv[0], argv, env) == -1)
             {
                 /**Verificar si el comando existe en las rutas especificadas en PATH**/
-                char *path_value = NULL;
-                char *path_name = "PATH=";
-                char **envp = environ;
+             
                 while (*envp != NULL) {
                     if (strncmp(*envp, path_name, strlen(path_name)) == 0) {
                         path_value = strchr(*envp, '=') + 1;
